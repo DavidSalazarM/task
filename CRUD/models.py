fad539b5c259ebac5ce7a9ff49eec1e29c468ac8
David Salazar <davidjosesalazarmora@gmail.com>
2021-09-10 15:40:27 -0500

    database and models

--- CRUD/models.py
+++ CRUD/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils.timezone import now
+import datetime
 
-# Create your models here.
+class MainTable(models.Model):
+    date_and_time_attention = models.DateTimeField(null=False, blank=False)
+    end_time_attention = models.TimeField(null=False, blank=False)
+    company = models.CharField(max_length=100, null=False, blank=False)
+    city = models.CharField(max_length=50, null=False, blank=False)
+    subject = models.CharField(max_length=100, null=False, blank=False)
+    answer =  models.TextField(null=False, blank=True)
+    application_date = models.DateField(default=datetime.date.today)
--- interview/settings.py
+++ interview/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'CRUD'
 ]
 
 MIDDLEWARE = [
@@ -75,11 +77,14 @@ WSGI_APPLICATION = 'interview.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'interview_tra',
+        'USER': 'david',
+        'PASSWORD': 'david',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
